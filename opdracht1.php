7ed8d00cfdbda9b037d6894eddc2849ea6ea149a
Tomraider47 <tom_97@live.com>
2015-09-18 12:09:04 +0200

    added opdracht

---
+++ opdracht1.php
@@ -0,0 +1,16 @@
+<?php
+$products = ['cola','sprite','orange','Monster'];
+foreach ($products as $product){
+	echo $product . '<br />';
+}
+
+/*$vehicles = [
+	['brand' => 'Mazda', 'type' => 'MX-5'],
+	['brand' => 'Volkswagen', 'type' => 'Golf']
+];
+
+foreach ($vehicles as $vehicle) {
+	echo $vehicle['brand'] . " " . $vehicle['type'];
+}
+*/
+?>
